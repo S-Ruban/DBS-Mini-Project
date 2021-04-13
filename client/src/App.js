00bb9a46886dafe20e4280b57ff10ba277f87223
Nandan H R <nandan.hr006@gmail.com>
2021-04-13 16:27:21 +0530

    Changed font to Quicksand

--- client/src/App.js
+++ client/src/App.js
@@ -9,32 +9,45 @@ import CustDashboard from './Components/Dashboards/CustDashboard';
 import RestDashboard from './Components/Dashboards/RestDashboard';
 import DelDashboard from './Components/Dashboards/DelDashboard';
 import Layout from './Components/Layout';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+	typography: {
+		fontFamily: 'Quicksand',
+		fontWeightLight: 400,
+		fontWeightRegular: 500,
+		fontWeightMedium: 600,
+		fontWeightBold: 700
+	}
+});
 
 function App() {
 	const user = useSelector((state) => state.user);
 
 	return (
-		<Router>
-			<Layout>
-				<Switch>
-					<Route exact path='/'>
-						{(user.uname && <Redirect to='/dashboard' />) ||
-							(!user.uname && <Redirect to='/signin' />)}
-					</Route>
-					<UnAuth exact path='/signin'>
-						<SignIn />
-					</UnAuth>
-					<UnAuth exact path='/signup'>
-						<SignUp />
-					</UnAuth>
-					<Auth exact path='/dashboard'>
-						{user.type === 'customer' && <CustDashboard />}
-						{user.type === 'restaurant' && <RestDashboard />}
-						{user.type === 'delivery' && <DelDashboard />}
-					</Auth>
-				</Switch>
-			</Layout>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Layout>
+					<Switch>
+						<Route exact path='/'>
+							{(user.uname && <Redirect to='/dashboard' />) ||
+								(!user.uname && <Redirect to='/signin' />)}
+						</Route>
+						<UnAuth exact path='/signin'>
+							<SignIn />
+						</UnAuth>
+						<UnAuth exact path='/signup'>
+							<SignUp />
+						</UnAuth>
+						<Auth exact path='/dashboard'>
+							{user.type === 'customer' && <CustDashboard />}
+							{user.type === 'restaurant' && <RestDashboard />}
+							{user.type === 'delivery' && <DelDashboard />}
+						</Auth>
+					</Switch>
+				</Layout>
+			</Router>
+		</ThemeProvider>
 	);
 }
 
